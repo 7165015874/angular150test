20a1f1826ba9c15427a452593ece62b4c2f966db
kushal <kushaldeveloper@gmail.com>
2017-11-02 13:11:09 -0400

    replace with forms
    https://angular.io/guide/forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './hero-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
